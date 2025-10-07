321e68a8f870b0794c7f90a098b9f2592b4f4718
lucki22813 <lucki22813@gmail.com>
2025-10-07 15:49:06 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-function greet(name) {
-  //  БАГ: якщо name не переданий — буде undefined
-  return "Hello, " + name.toUpperCase();
-}
-
-console.log(greet()); // викликається без аргументу -> помилка
