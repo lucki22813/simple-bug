5cba00e978c090425ec54ddb54b190515a2600f2
lucki22813 <lucki22813@gmail.com>
2025-10-07 15:49:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function greet(name) {
+  // БАГ: якщо name не переданий — буде undefined
+  return "Hello, " + name.toUpperCase();
+}
+
+console.log(greet()); // викликається без аргументу -> помилка
